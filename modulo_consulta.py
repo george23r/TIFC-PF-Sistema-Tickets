62d79efc148607f6336efb88a85c3d1942bd61c6
Gerarcore <grv1405@gmail.com>
2026-05-13 11:10:15 -0500

    Agregue hola mundo Gerardo

--- modulo_consulta.py
+++ modulo_consulta.py
@@ -2,4 +2,4 @@ def listar_tickets(filtro=None):
     # Lee tickets línea por línea
     # Muestra en formato de tabla por consola
     # filtro puede ser "Abierto", "Cerrado", "Alta", etc.
-    pass
+    print ("Hola mundo desde consulta-Gerardo")
