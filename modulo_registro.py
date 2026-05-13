93c39480d6d5a4860772a4e233e8a5153955461d
george23r <112.jor1020@gmail.com>
2026-05-13 10:55:11 -0500

    Update modulo_registro.py

--- modulo_registro.py
+++ modulo_registro.py
@@ -1,5 +1,9 @@
 def registrar_ticket(descripcion, prioridad):
+    print("hola mundo")
     # Valida que descripcion no esté vacía
+    if descripcion == "" or prioridad == "":
+        print("Error: Descripción y prioridad son obligatorias.")
+        return
     # Genera ID: "T-001", "T-002"...
     # Escribe en el diccionario
     print("hola mundo")
